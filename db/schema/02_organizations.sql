6e4ab1431a6aa7bae7f88fee24f0d8a1fa4d1be1
WillWSmith <will.w.smith94@gmail.com>
2024-02-27 10:00:02 -0500

    Add name column to organizations

--- db/schema/02_organizations.sql
+++ db/schema/02_organizations.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS organizations CASCADE;
 CREATE TABLE organizations (
   id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
   user_id INTEGER NOT NULL,
   account_id INTEGER NOT NULL
 );
