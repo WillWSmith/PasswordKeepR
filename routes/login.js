a578685c9bbcc74518ea81e8f730fc724e29cff1
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-02-28 13:51:22 -0500

    add router.get to render login form, not sure if necessary

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router  = express.Router();
 
-// do this instead
-router.post('/login', (req, res) => {
-  const email = req.body.email;
+
+router.get('/', (req, res) => {
+  const user_email = req.cookies.user_email; 
+  const templateVars = { user_email };
  
+  if (!user_email) {
+    return res.redirect('/login');
+  }
+  res.render('login', templateVars);
+});
+
+
+// Login
+router.post('/login', (req, res) => {
+  const user_email = req.body.user_email;
+ 
+  if (!user_email) {
+    return res.status(400).send('email is required');
+  }
   // or using plain-text cookies, set to user's email
-  res.cookie('user_email', req.body.email);
+  res.cookie('user_email', req.body.user_email);
 
   // send the user somewhere
   res.redirect('/index');
