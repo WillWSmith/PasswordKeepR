c08b3e3bf9a09e1205c867c969042bcaee47ad92
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-02-28 14:49:24 -0500

    create generatePassword()

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,36 @@
 // Client facing scripts here
+
+//Do we use jQuery or vanilla JS?
+
+const generatePassword = function (length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberChars = '0123456789';
+  const symbolChars = '!@#$%^&*()-_=+[{]}|;:,<.>/';
+
+  let chars = '';
+  
+  if (includeLowercase) {
+    chars += lowercaseChars;
+  }
+  if (includeUppercase) {
+    chars += uppercaseChars;
+  }
+  if (includeNumbers) {
+    chars += numberChars;
+  }
+  if (includeSymbols) {
+    chars += symbolChars;
+  }
+  if (chars === '') {
+    alert('Please select at least one character type');  
+  }
+
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    let randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  return password;
+};
