6a9b89adb5611986d2b5f4c8c6087d43c22f5d29
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-02-29 10:10:07 -0500

    modify route to include password

--- routes/login.js
+++ routes/login.js
@@ -3,22 +3,21 @@ const router  = express.Router();
 const db = require('../db/connection');
 
 
-// Login
 router.post('/login', (req, res) => {
-  const query = `SELECT * FROM users WHERE email = $1`;
-  const values = [req.body.email];
+  const query = `SELECT * FROM users WHERE email = $1 AND password = $2`;
+  const values = [req.body.email, req.body.password];
   
   db.query(query, values)
     .then(data => {
-      const email = data.rows[0];
-      
-      if (email === req.body.email) {
-        res.cookie('user_email', req.body.email);
-        res.redirect('/index');
+      if (data.rows.length > 0) {
+        // if user with email and password found
+        const user = data.rows[0];
+          res.cookie('user_email', req.body.email);
+          res.redirect('/index');
   
-      } else {
-        res.status(401).send('Unauthorized');
-      }
+        } else { // no user found with email and password
+          res.status(401).send('Unauthorized');
+        }
     })
     .catch(err => {
       res
