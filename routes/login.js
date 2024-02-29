e56976096b8e7f1ef5d4778106750ea24a265d56
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-02-29 08:55:46 -0500

    query to db to check email and set cookie

--- routes/login.js
+++ routes/login.js
@@ -1,28 +1,40 @@
 const express = require('express');
 const router  = express.Router();
+const db = require('../db/connection');
 
-
-router.get('/', (req, res) => {
-  const user_email = req.cookies.user_email; 
-  const templateVars = { user_email };
+// router.get('/', (req, res) => {
+//   const user_email = req.cookies.user_email; 
+//   const templateVars = { user_email };
  
-  if (!user_email) {
-    return res.redirect('/login');
-  }
-  res.render('login', templateVars);
-});
+//   if (!user_email) {
+//     return res.redirect('/login');
+//   }
+//   res.render('login', templateVars);
+// });
 
 
 // Login
 router.post('/login', (req, res) => {
-  const user_email = req.body.user_email;
- 
-  if (!user_email) {
-    return res.status(400).send('email is required');
-  }
-  // or using plain-text cookies, set to user's email
-  res.cookie('user_email', req.body.user_email);
-
-  // send the user somewhere
-  res.redirect('/index');
+  const query = `SELECT * FROM users WHERE email = $1`;
+  const values = [req.body.email];
+  
+  db.query(query, values)
+    .then(data => {
+      const email = data.rows[0];
+      
+      if (email === req.body.email) {
+        res.cookie('user_email', req.body.email);
+        res.redirect('/index');
+  
+      } else {
+        res.status(401).send('Unauthorized');
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
 });
+ 
+module.exports = router;
