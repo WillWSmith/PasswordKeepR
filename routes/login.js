89886cfee23ef427e4d6dd7c24832d971250c1ba
WillWSmith <will.w.smith94@gmail.com>
2024-03-01 08:12:23 -0500

    reADD authorization

--- routes/login.js
+++ routes/login.js
@@ -1,24 +1,25 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const db = require('../db/connection');
-
 
 router.post('/login', (req, res) => {
   const query = `SELECT * FROM users WHERE email = $1 AND password = $2`;
   const values = [req.body.email, req.body.password];
-  
+
   db.query(query, values)
     .then(data => {
       if (data.rows.length > 0) {
         // if user with email and password found
-        const user = data.rows[0];
         res.cookie('user_email', req.body.email);
+        res.redirect('/index'); // Redirect only if login is successful
+      } else {
+        // no user found with email and password
+        res.status(401).send('Unauthorized');
       }
-      res.redirect('/index');
     })
     .catch(err => {
       res.status(500).json({ error: err.message });
     });
 });
- 
+
 module.exports = router;
