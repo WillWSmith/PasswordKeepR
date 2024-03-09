00be6b586eacc90ec822f0f6b25288110b08eb51
WillWSmith <will.w.smith94@gmail.com>
2024-03-09 10:03:34 -0500

    Minor seeding change

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, password) VALUES ('Alice', 'Alice@gmail.com', 1111);
+INSERT INTO users (name, email, password) VALUES ('Marie', 'Marie@gmail.com', 1111);
 INSERT INTO users (name, email, password) VALUES ('Kira', 'Kira@gmail.com', 1111);
 INSERT INTO users (name, email, password) VALUES ('Josh', 'Josh@gmail.com', 1111);
 INSERT INTO users (name, email, password) VALUES ('Molly', 'Molly@gmail.com', 1111);
